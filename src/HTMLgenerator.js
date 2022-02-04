0c962e0416fa90746e1187f512872e38e7c3a9b0
Mauro Vasconcelos <88060989+vanillainanutshell@users.noreply.github.com>
2022-02-04 17:58:26 -0200

    Add API function

--- src/HTMLgenerator.js
+++ src/HTMLgenerator.js
@@ -1,17 +1,17 @@
-const HTMLgenerator = () => {
-  const containerUl = document.getElementById('scoreUl');
-  let counter = 0;
-  containerUl.innerHTML = '';
-  for (let i = 0; i < 3; i += 1) {
-    const li = document.createElement('li');
-    counter += 1;
-    li.innerHTML = `<li class="gameLi"><p>game ${counter}</p></li>`;
-    li.classList.add('taskLi');
-    if (counter % 2 !== 0) {
-      li.classList.add('grey');
-    }
-    containerUl.appendChild(li);
-  }
+const setScore = (scoreList) => {
+    const containerUl = document.getElementById('scoreUl');
+    let counter = 0;
+    containerUl.innerHTML = '';
+    scoreList.result.forEach((element) => {
+        const li = document.createElement('li');
+        counter += 1;
+        li.innerHTML = `<li class="gamesLi"><div><p>USER: ${element.user}</p></div><div></div><p>SCORE: ${element.score}</p></li>`;
+        li.classList.add('taskLi');
+        if (counter % 2 !== 0) {
+            li.classList.add('grey');
+        }
+        containerUl.appendChild(li);
+    });
 };
 
-export default HTMLgenerator;
+export default setScore;
