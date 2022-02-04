00e613dc95a7878a264a116f4c117d1b877fbf0f
Mauro Vasconcelos <88060989+vanillainanutshell@users.noreply.github.com>
2022-02-04 17:59:32 -0200

    Linters

--- src/HTMLgenerator.js
+++ src/HTMLgenerator.js
@@ -1,17 +1,17 @@
 const setScore = (scoreList) => {
-    const containerUl = document.getElementById('scoreUl');
-    let counter = 0;
-    containerUl.innerHTML = '';
-    scoreList.result.forEach((element) => {
-        const li = document.createElement('li');
-        counter += 1;
-        li.innerHTML = `<li class="gamesLi"><div><p>USER: ${element.user}</p></div><div></div><p>SCORE: ${element.score}</p></li>`;
-        li.classList.add('taskLi');
-        if (counter % 2 !== 0) {
-            li.classList.add('grey');
-        }
-        containerUl.appendChild(li);
-    });
+  const containerUl = document.getElementById('scoreUl');
+  let counter = 0;
+  containerUl.innerHTML = '';
+  scoreList.result.forEach((element) => {
+    const li = document.createElement('li');
+    counter += 1;
+    li.innerHTML = `<li class="gamesLi"><div><p>USER: ${element.user}</p></div><div></div><p>SCORE: ${element.score}</p></li>`;
+    li.classList.add('taskLi');
+    if (counter % 2 !== 0) {
+      li.classList.add('grey');
+    }
+    containerUl.appendChild(li);
+  });
 };
 
 export default setScore;
--- src/api.js
+++ src/api.js
@@ -3,28 +3,28 @@ import setScore from './HTMLgenerator.js';
 const form = document.getElementById('submitBtn');
 const refreshBtn = document.getElementById('refreshBtn');
 
-const addPlayer = async(e) => {
-    console.log('batata');
-    e.preventDefault();
-    const infoInput = document.querySelectorAll('input');
-    const response = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/yP3JQos7EWB0TjhGmzyM/scores/', {
-        method: 'POST',
-        body: JSON.stringify({
-            user: infoInput[0].value,
-            score: Number(infoInput[1].value),
-        }),
-        headers: {
-            'Content-type': 'application/json; charset=UTF-8',
-        },
-    });
-    infoInput[0].value = '';
-    infoInput[1].value = '';
-    return response.json();
+const addPlayer = async (e) => {
+  console.log('batata');
+  e.preventDefault();
+  const infoInput = document.querySelectorAll('input');
+  const response = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/yP3JQos7EWB0TjhGmzyM/scores/', {
+    method: 'POST',
+    body: JSON.stringify({
+      user: infoInput[0].value,
+      score: Number(infoInput[1].value),
+    }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  });
+  infoInput[0].value = '';
+  infoInput[1].value = '';
+  return response.json();
 };
 
-const getScore = async() => {
-    const response = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/yP3JQos7EWB0TjhGmzyM/scores/');
-    return response.json();
+const getScore = async () => {
+  const response = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/yP3JQos7EWB0TjhGmzyM/scores/');
+  return response.json();
 };
 
 getScore().then((response) => { setScore(response); });
